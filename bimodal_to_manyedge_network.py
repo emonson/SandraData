baefcf212ba94634c5c6ae39ca8dd61e18df7f4a
Eric E Monson <emonson@cs.duke.edu>
2014-07-25 14:38:29 -0400

    mod of main app for new data, plus trying to do all edges instead of aggregating so can associate attributes with each edge. seems to work but really slow...

--- bimodal_to_manyedge_network.py
+++ bimodal_to_manyedge_network.py
@@ -16,8 +16,9 @@ data_file = 'Paintings_Shipments_AGI_v3_UpdatesRefine_V4.xls'
 sheet_name = 'PaintingsShipments'
 person_col = 'AGENT_NAME'
 item_col = 'ID_Reg'
-out_file = 'PersonShipments_edges.tsv'
-
+out_file = 'PersonShipments_manyedges.tsv'
+attributes_list = ['ID_Reg', 'YEAR', 'FOLIO', '#Pntgs', 'Paintings', 'Sculptures', 'Prints', 'Books', 'Painters_Mats', 'Religious_goods', 'Gral_Merch', 'Religious_Order']
+any_not_null_cols = ['Religious_Order']
 
 xl = pd.ExcelFile(os.path.join(data_dir, data_file))
 df = xl.parse(sheet_name)
@@ -45,19 +46,34 @@ i_idxs = [i_lookup[ii] for ii in df[item_col]]
 
 bimodal_edge_mtx = coo_matrix((np.ones_like(p_idxs),(p_idxs,i_idxs)), shape=(len(p_unique),len(i_unique))).tocsr()
 
-unimodal_edge_mtx = bimodal_edge_mtx * bimodal_edge_mtx.T
+# Now need to generate a copy of the spreadsheet with unique entries for the item/shipment/transaction
+gg = df.groupby(df[item_col])
+any_not_null = lambda x: pd.notnull(x).any()
+# For most of the columns we'll assume all entries are duplicates in our desired attribute rows
+dfg = gg[attributes_list].first()
+# But for some we may want to apply other aggregation functions
+dfg[any_not_null_cols] = gg[any_not_null_cols].aggregate(any_not_null)
 
-# Only recording one direction so need to specify bidirectional in gephi
-triu_uni_edge_coo = triu(unimodal_edge_mtx.tocoo())
-rcv = zip(triu_uni_edge_coo.row, triu_uni_edge_coo.col, triu_uni_edge_coo.data)
+graph_edges_str = []
+edge_count = 0
 
-# vt is the value of the edge coming from the target back towards the source
-# which we need for some edge rendering schemes
-# excluding self-edges
-# graph_edges = [{'source':int(r), 'target':int(c), 'v':int(v), 'vt':int(unimodal_edge_mtx[c,r]), 'i':int(i)} for i,(r,c,v) in enumerate(zip(uni_edge_coo.row, uni_edge_coo.col, uni_edge_coo.data)) if r != c]
+# Now do explicit double loop through persons
+for p1 in range(len(p_unique)):
+    print p1, len(p_unique)
+    for p2 in range(p1+1,len(p_unique)):
+        pp_arr = np.multiply(bimodal_edge_mtx[p1,:].toarray().flatten(),bimodal_edge_mtx[p2,:].toarray().flatten())
+        for item_id in i_unique[pp_arr > 0]:
+            edge = dfg.loc[item_id,:].to_dict()
+            edge['source'] = p_rev_lookup[p1]
+            edge['target'] = p_rev_lookup[p2]
+            edge['weight'] = 1
+            edge['id'] = edge_count
+            edge['type'] = 'Undirected'
+            graph_edges_str.append(edge)
+            edge_count = edge_count + 1
+        
 
-graph_edges = [{'source':int(r), 'target':int(c), 'weight':int(v), 'id':int(i), 'type':'Undirected'} for i,(r,c,v) in enumerate(rcv) if r != c]
-graph_edges_str = [{'source':p_rev_lookup[r], 'target':p_rev_lookup[c], 'weight':int(v), 'id':int(i), 'type':'Undirected'} for i,(r,c,v) in enumerate(rcv) if r != c]
+# graph_edges_str = [{'source':p_rev_lookup[r], 'target':p_rev_lookup[c], 'weight':int(v), 'id':int(i), 'type':'Undirected'} for i,(r,c,v) in enumerate(rcv) if r != c]
 
 grdf = pd.DataFrame(graph_edges_str)
 grdf.index.name = 'idx'
